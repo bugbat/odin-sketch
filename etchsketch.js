baa5f77d845259a49ab8016cf3b8fe9fc149e226
bugbat <85291787+bugbat@users.noreply.github.com>
2024-04-01 22:16:44 -0400

    add random color values and

--- etchsketch.js
+++ etchsketch.js
@@ -2,6 +2,11 @@
 function makeGrid(x, y) {
   const wrapper = document.querySelector("#wrapper");
   const row = document.createElement("div");
+
+  //clear grid
+  while(wrapper.hasChildNodes()) {
+    wrapper.removeChild(wrapper.firstChild);
+  }
   
   row.classList.add("row");
 
@@ -15,16 +20,50 @@ function makeGrid(x, y) {
     clone = row.cloneNode(true);
     wrapper.appendChild(clone);
   }
+  setHover();
 }
 
-// swap colors on mouseover
+// set mouseover interaction
 function setHover() {
   const blocks = document.querySelectorAll(".block");
   blocks.forEach(block =>
     block.addEventListener("mouseover", function() {
-      block.classList.add("block-alt");
+      if (!block.style.opacity) {
+        block.style.opacity = "1";
+        block.style.backgroundColor = "rgb(" + getColorValue() + ", " + getColorValue() + ", " + getColorValue() + ")"; // messy duplication of statements. fix later
+      }
+      else if (block.style.opacity > 0){
+        block.style.opacity -= 0.1;
+        block.style.backgroundColor = "rgb(" + getColorValue() + ", " + getColorValue() + ", " + getColorValue() + ")";
+      }
     }));
   }
 
+// random rgb value
+function getColorValue() {
+  return Math.floor(Math.random() * 255).toString();
+}
+
+
+// prompt for grid creation
+function gridPrompt() {
+  const gridSize = prompt("Enter the size of your new grid. The max size is 100.");
+  if (!parseInt(gridSize)){
+    gridPrompt();
+  }
+  else if (gridSize > 100) {
+    gridPrompt();
+  }
+  else {
+    makeGrid(parseInt(gridSize), parseInt(gridSize));
+  }
+}
+
+// set up button to create a new grid
+const newGridBtn = document.querySelector("button");
+newGridBtn.addEventListener("click", function() {
+  gridPrompt();
+})
+
+// Initial setup
 makeGrid(16, 16);
-setHover();
